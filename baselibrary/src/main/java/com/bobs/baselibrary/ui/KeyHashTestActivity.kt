96d25c04aa53ce9fda1abe0dbd14abf9f6eb2fe5
kmg8787 <gmg8787@naver.com>
2020-03-29 20:08:28 +0900

    add baselibrary

---
+++ baselibrary/src/main/java/com/bobs/baselibrary/ui/KeyHashTestActivity.kt
@@ -0,0 +1,22 @@
+package com.bobs.baselibrary.ui
+
+import android.os.Bundle
+import com.bobs.baselibrary.R
+import com.bobs.baselibrary.base.BaseActivity
+import com.bobs.mapque.keyhash.KeyHashViewModel
+
+//class KeyHashTestActivity : BaseActivity<ActivityKeyhashBinding>() {
+//
+//    override val layoutId: Int
+//        get() = R.layout.activity_keyhash
+//
+//    val keyHashViewModel : KeyHashViewModel = KeyHashViewModel()
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        keyHashViewModel.setKeyHash(this)
+//
+//        viewDataBinding.model = keyHashViewModel
+//    }
+//}
